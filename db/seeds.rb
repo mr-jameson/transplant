da3ec079f29438ca2d9c5e16835b009217e72a5a
mr-jameson <elliot.jameson4@gmail.com>
2019-10-30 10:14:49 +1100

    working on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Create random users
 user_ids = []
-
-for i in 1..10
+for i in 1..20
     user = User.create(
-        name: "user#{i}"
+        name: Faker::Name.name
         email: "test#{i}@test.com",
-        password: "password#{i}"
+        password: "Testing123"
+        mobile: "0#{rand(408000000 - 499999999)}"
         description: "I like plants"
+        created_at: Time.now
     )
     p "Created #{i} users!"
 
@@ -21,16 +23,33 @@ for i in 1..10
 
 end
 
+listings = [
+    {name: "Philodendron Hope", species: "Philodendron Hope", pot: "Plastic planter", light: 2, size: 1, description: "Philodendron Hope are versatile indoor or shade plants with large, tropical looking foliage with leaves that look like Elephants ears. They can handle cold climates, but prefer their natural warm rainforest weather. Hope need bright, indirect sunlight to thrive indoors and keep their foliage looking full and lush.", }
+]
 
-for i in 1..10
+for i in 1..20
     Plant.create(
-        name: "Plant#{i}",
-        price: rand(500..1000)
-        description: "Plant#{i} is healthy",
+        name: listings[:title],
+        price: rand(500..1000),
+        description: listings[:description],
         created_at: Time.now,
         postage: "Local Pickup"
+        species: listings[:species]
+        pot: listings[:species]
+        light: rand(0..2) #enum 0 low, 1 moderate, 2 bright
+        size: rand(0..2) #enum 0 small, 1 medium, 2 large
         user_id: user_ids.sample,
     )
-
     p "Created #{i} plants!"
 end
+
+for i in 1..20
+    Address.create(
+        user_id: user_ids.sample
+        address:
+        suburb:
+        state: "NSW"
+        postcode:
+    )
+end
+
