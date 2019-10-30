759823be51b52afe1df4db40323e9da061ebcea1
mr-jameson <elliot.jameson4@gmail.com>
2019-10-31 09:37:07 +1100

    assigned suburb to plant index page

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,6 @@
 class Plant < ApplicationRecord
   belongs_to :user
+  has_one :address, through: :user
   enum light: { low: 0, moderate: 1, bright: 2 }
   enum size: { small: 0, medium: 1, large: 2 }
   validates :name, :price, :species, :light, :size, :description, :image, presence: true
