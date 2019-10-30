847b7ca6b3feb633572d39eb607637354f27c552
mr-jameson <elliot.jameson4@gmail.com>
2019-10-30 15:12:50 +1100

    setup create plant view page

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,7 +1,8 @@
 class PlantsController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_plant, only: [:show]
+    before_action :set_plant, only: [:show, :edit, :update, :destroy]
     before_action :set_user_plant, only: [:edit, :update]
+    # investgate set_user_plant?
 
     def index
         @plant = Plants.all
@@ -9,9 +10,17 @@ class PlantsController < ApplicationController
 
     def new
         @plant = Plant.new
+        @address = Address.all
     end
 
     def create
+        @plant = current_user.plants.create(plant_params)
+        
+        if @plant.errors.any?
+            render "new"
+        else 
+            redirect_to plant_path(@plant)
+        end
     end
 
     def show
@@ -22,4 +31,23 @@ class PlantsController < ApplicationController
 
     def destroy
     end
+
+    private
+    def plant_params
+        params.require(:plant).permit(:name, :price, :description, :light, :size, :species, :pot, :postage, :image)
+    end
+
+    def set_plant
+        id = params[:id]
+        @plant = Plant.find(id)
+    end
+
+    def set_user_plant
+        @milkshake = current_user.plants.find_by_id(params[:id])
+
+        if @plant == nil
+            redirect_to plants_path
+        end
+    end
+
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :user
+  enum light: { low: 0, moderate: 1, bright: 2 }
+  enum size: { small: 0, medium: 1, large: 2 }
+  validates :name, :price, :species, :light, :size, :description, :image, presence: true
   has_one_attached :image
-  enum light: {low: 0, moderate: 1, bright:2}
-  enum size: {small:0, medium:1, large:2}
 end
