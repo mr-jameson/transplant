c3684cdd837684c7472c56de4585ddaccc328bb2
mr-jameson <elliot.jameson4@gmail.com>
2019-11-01 10:37:38 +1100

    added stripe payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,16 @@
 class PaymentsController < ApplicationController
     def success
     end
+
+    def webhook
+        payment_id= params[:data][:object][:payment_intent]
+        payment = Stripe::PaymentIntent.retrieve(payment_id)
+        plant_id = payment.metadata.plant_id
+        user_id = payment.metadata.user_id
+    
+        p "plant id " + plant_id
+        p "user id " + user_id
+    
+        status 200
+    end
 end
