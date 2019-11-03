5e183351043c6a575e6fba73cf9b914ab90532d1
mr-jameson <elliot.jameson4@gmail.com>
2019-11-04 10:23:07 +1100

    connected sign-up form to database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-    private
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:mobile, :description, address_attributes: [:address, :state, :suburb, :postcode]])
+    end 
+  
+  private
 
     # Overwriting the sign_out redirect path method
     def after_sign_out_path_for(resource_or_scope)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_one :address
   has_many :plants
   has_one_attached :profile_pic
+  accepts_nested_attributes_for :address
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
