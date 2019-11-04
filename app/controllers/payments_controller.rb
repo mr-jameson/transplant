9c91dc9e2c16628b547ca08e535709b166f17dca
mr-jameson <elliot.jameson4@gmail.com>
2019-11-04 13:16:11 +1100

    troubleshooting bugs

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,9 +2,8 @@ class PaymentsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:webhook]
 
     def success
-        id = params[:id]
+        id = params[:plantId]
         @plant = Plant.find(id)
-        byebug
     end
 
     def webhook
@@ -12,9 +11,9 @@ class PaymentsController < ApplicationController
         payment = Stripe::PaymentIntent.retrieve(payment_id)
         plant_id = payment.metadata.plant_id
         user_id = payment.metadata.user_id
-    
+        p "**************************************************************"
+        p payment.metadata
         Ledger.create(plant_id: plant_id, user_id: user_id, stripe_id: payment.id )
-    
         status 200
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "pages#home", as: "root"
   get "/plants", to: "plants#index", as: "plants"
